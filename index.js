917729b43d891100acc243f85aaad19eb20dd197
gustavosalvadoralvarez <gustavosalvadoralvarez@gmail.com>
2016-01-12 01:44:58 -0600

    implementation following McCarthy's List Programmer's Manual 1.5, with the addition of hash-based namespacing

--- index.js
+++ index.js
@@ -1,72 +1,315 @@
 
-// a. A Class of Symbolic Expressions (in Javascript)
+// jshint esnext:true
 
-const s = (...args) => (args);
+const NILL = Symbol("NILL");
 
-const c = (s1, s2, s3) => s1 ? s2 : s3;
+const sexpr = (head, tail) => [head, (tail ? tail : NILL)];
 
-// b. Functions of S-expressions and the Expressions That Represent Them.
+const atom = (x) => sexpr(x);
 
-const f = (fn, ...args) => fn(...args);
+const cons = sexpr;
 
-// c. Elementary S-Functions and Predicates
+const string$ = (x) => x instanceof String;
 
-const apply = (method, v, ...args) => f(method.apply, v, args);
+const number$ = (x) => x instanceof Number;
 
-const val = (v, k) => c((k in v), (v[k]), undefined);
+const regexp$ = (x) => x instanceof RegExp;
 
-const len = (s1) => f(val, s(...s1), "length");
+const array$ = (x) => Array.isArray(x);
 
-const slice = (s1, ...args) => f(apply, [].slice, s(...s1), args);
+const object$ = (x) => !array$(x) && (x instanceof Object));
 
-const mapcar = (...args, s1) => f(apply, [].map, s(...s1), args);
+const map$ = (x) => x instanceof Map;
 
-const fldr = (...args, s1) => f(apply, [].reduce, s(...s1), args);
+const symbol$ = (x) => x instanceof Symbol
 
-const sf = (fn) => (...args) => f(mapcar, fn, args);
+const set$ = (x) => x instanceof Set;
 
-const atom = (v) => c(v, (f(len, s(...arguments)) === 1), undefined);
+const lambda$ = (x) => x instanceof Function;
 
-const atoms = sf(atom);
+const label$ = (x) => string$(x) && (x in NS);
 
-const not = (v) => c(f(atom, v), (!v), undefined);
+// atom [X] = atom [(X · NILL)] = T
+// atom [(X · A)] = F
+const atom$ = (x) => {
+  if (!array$(x)) return true;
+  let head = x[0],
+      tail = x.length > 1 ? x.slice(1) : null;
+  return (!tail || tail === NILL);};
 
-const nots = sf(not);
+// eq$ [X; X] = T
+// eq$ [X; A] = F
+// eq$ [X; (X · A)] is undefined.
+const eq$ = (x, y) => !(!atom$(x) || !atom$(y)) ? undefined : (x[0] === y[0]);
 
-const notAtom = (v) => f(not, f(atom, v));
+// car [(X · A)] = X
+// car [((X · A) · Y )] = (X · A
+const car = (x) => atom$(x) ? undefined : x[0];
 
-const notAtoms = sf(notAtom);
+const cdr = (x) => {
+  return atom$(x) ?
+          undefined :
+          x.length > 1 ?
+            1 in x ? x[1] :
+            undefined;};
 
-const or = (v1,  v2) => c(f(or, s(f(notAtoms, s(v1, v2)))),
-                          undefined,
-                          (v1 || v2));
+const caar = (x) => car(car(x));
 
-const and = (v1, v2) => c(f(and, ...f(atoms, s(v1, v2))),
-                          undefined,
-                          (v1 && v2));
+const cadr = (x) => car(cdr(x));
 
-const xor = (v1, v2) => f(and, f(or, v1, v2), f(not, f(and, v1, v2)));
+const caddr = (x) => car(cdr(cdr(x)));
 
-const eq = (v1, v2) => c(f(or, ...f(notAtoms, s(v1, v2)),
-                         undefined,
-                         (v1 === v2));
+const cddr = (x) => cdr(cdr(x));
 
-const car = (s1) => c(f(atom, s1), undefined, f(slice, s1, 0, 1));
+const list$ = (x) => {
+  let rest = cdr(x);
+  return array$(x) && !atom$(x) && (list$(rest) || atom$(rest));
+};
 
-const cdr = (s1) => c(f(atom, s1), undefined, f(slice, s1, 1));
+// ff[x] = [atom[x] → x; T → ff[car[x]]]
+const ff = function ff(x){
+  return (atom$(x) ?
+            head(x) :
+            ff(car(x)))};
 
-const cons = (s1, s2) => s(...s1, s2);
+// subst [x; y; z] = [atom [z] →
+//                     [eq$ [z; y] → x;
+//                     T → z];
+//                    T →
+//                     cons [subst [x; y; car [z]]; subst [x; y; cdr [z]]]]
+const subst = function subst(x, y, z){
+  return (atom$(z) ?
+            (eq$(z, y) ? x : z) :
+            cons(subst(x, y, car(z),
+               subst(x, y, cdr(z)))))};
 
-const ff = (s1) => c(f(atom, s1), s1, f(ff, f(car, s1)));
+// eq$ual [x; y] = [atom [x] ∧ atom [y] ∧ eq$ [x; y]] ∨
+//                [¬ atom [x] ∧¬ atom [y] ∧ eq$ual [car [x]; car [y]] ∧
+//                 eq$ual [cdr [x]; cdr [y]]]
+const equal$ = function eq$ual(x, y){
+  return (atom$(x) && atom$(y) && eq$(x, y)) ||
+         (!atom$(x) && !atom$(y) && equal(car(x), car(y)) && equal(cdr(x), cdr(y)))};
 
-const subst = (s1, sym, s2) => c(f(atom, s2),
-                                 f(eq, sym, s2),
-                                 f(cons,
-                                   f(subst, s1, sym, f(car, s2)),
-                                   f(subst, s1, sym, f(cdr, s2))));
+// null[x] = atom[x] ∧ eq$[x; NIL]
+const nill$ = (x) => !(!atom$(x) || !eq$(tail(x), NILL) || empty$(x));
 
-const equal = (s1, s2) => c(f(and, f(atoms, s(s1, s2))),
-                            f(eq, s1, s2),
-                            f(and,
-                              f(equal, f(car, s1), f(car, s2),
-                              f(equal, f(cdr, s1), f(cdr, s2)))));
+const empty$ = (x) => !array$(x) || !x.length || nill$(x));
+
+
+const list = function (head, ...tail) {
+  return tail[0] === close ? head : cons(head, list(...tail));
+
+const close = Symbol(")");
+
+// append [x; y] = [null[x] → y;
+//                  T → cons [car [x];
+//                            append [cdr [x]; y]]]
+const append = function append(x, y){
+  return (nill$(x) ?
+            y :
+            cons(car(x),
+                 append(cdr(x), y))))};
+
+ const member$ = function(x, y){
+   return  nill$(y) ?
+            !!0 :
+            eq$ual$(x, car(y)) ?
+              !0 :
+              member$(x, cdr(y));}
+
+// among[x; y] = ¬null[y] ∧
+//               [eq$ual[x; car[y]] ∨
+//               among[x; cdr[y]]]
+const among$ = function among(x, y){
+  return !nill$(x) &&
+          (eq$ual(x, car(y) ||
+          among(x, cdr(y))));};
+
+// pair[x; y] = [null[x]∧null[y] → NIL;
+//               ¬atom[x]∧¬atom[y] → cons[list[car[x]; car[y]];
+//               pair[cdr[x]; cdr[y]]]
+const pair = function pair(x, y){
+  return nill$(x) && nill$(y) ?
+            NILL :
+            !(!atom$(x) || !atom$(y)) ?
+                cons(list(car(x), car(y)) :
+                     pair(cdr(x), cdr(y)))};
+
+// assoc[x; y] = eq$[caar[y]; x] → cadar[y];
+//               T → assoc[x; cdr[y]]]
+const assoc = function assoc(x, y){
+  return eq$(caar(y), x) ?
+            cadar(y) :
+            assoc(x, cdr(y));};
+
+// sub2[x; z] = [null[x] → z;
+//               eq$[caar[x]; z] → cadar[x];
+//              T → sub2[cdr[x]; z]]
+const sub2 = function(x, z){
+  return nill$(x) ?
+            y :
+            eq$(caar(x), z) ?
+              cadar(x) :
+              sub2(cdr(x), z);};
+
+// sublis[x; y] = [atom[y] → sub2[x; y];
+//                T → cons[sublis[x; car[y]]; sublis[x; cdr[y]]]
+const sublis = function sublis(x, y){
+  return atom$(y) ?
+            sub2(x, y) :
+            cons(sublis(x, car(y),
+                 sublis(x, cdr(y))));};
+
+const pairlis = function pairlis(x, y, a){
+  return empty$(x) ?
+            a :
+            cons(cons(car(x), car(y)),
+                 pairlis(cdr(x), cdr(y), a));};
+
+const quote = (x) => {
+  x.QUOTED = true;
+  return () => x;};
+
+const quote$ = (x) x && x.QUOTED;
+
+const ENV = new Map();
+
+const NS = {};
+
+const def = (label, x) => {
+  const sym = Symbol(label);
+  ENV.set(sym, x);
+  NS[label] = sym;
+  return null;};
+
+const LAMBDA = Symbol("LAMBDA");
+
+const QUOTE = Symbol("QUOTE");
+
+const LABEL = Symbol("LABEL");
+
+const COND = Symbol("COND");
+
+const defn = (name, fn) => def(name, cons(LAMBDA, fn));
+
+const not = (x) => !x;
+
+const and = function and(n, ...r){
+  return n && (empty$(r) ? true : and(...r));};
+
+const or = function or(n, ...r){
+  return n || (empty$(r) ? false : or(...r));};
+
+const xor = function xor(n, ...r){
+  return and(or(n, ...r), not(and(n, ...r)));
+
+const sub = function sub(n, ...r){
+  return n - (empty$(r) ? 0 : sub(...r));};
+
+const add = function add(n, ...r){
+  return n + (empty$(r) ? 0 : add(...r));};
+
+const sub = function sub(n, ...r){
+  return n - (empty$(r) ? 0 : sub(...r));};
+
+const mult = function mult(n, ...r){
+  return n * (empty$(r) ? 1 : mult(...r));};
+
+const mod = (x, y) => x%y;
+
+const exp = (x, y) => Math.pow(x, y);
+
+def("define", def);
+def("function", defn);
+def("(", list);
+def(")", close);
+def("T", true);
+def("F", false);
+def("~", not);
+def("and", and);
+def("or", or);
+def("xor", xor);
+def("+", add);
+def("-", sub);
+def("*", mult);
+def("EXP", exp);
+def("%", mod);
+def("lambda", LAMBDA);
+def("quote", QUOTE);
+def("label", LABEL);
+def("cond", COND);
+def("NILL", NILL);
+def("sexpr", sexpr);
+def("atom", (x) => atom(car(x)));
+def("cons", (x) => cons(car(x), cadr(x)));
+def("car", (x) => caar(x));
+def("cdr", (x) => cdar(x));
+def("eq?", (x) => eq$(car(x), cadr(x)));
+def("string?", string$);
+def("number?", number$);
+def("regexp?", regexp$);
+def("array?", array$);
+def("object?", object$);
+def("map?", map$);
+def("symbol?", symbol$);
+def("set?", set$);
+def("lambda?", lambda$);
+def("label?", label$);
+def("atom?", atom$);
+def("caar", caar);
+def("cadr", cadr);
+def("caddr", car);
+def("caddr", caddr);
+def("list?", list$);
+
+
+function apply(fn, x, a){
+  let fncar = car(fn),
+      fncdr = cdr(fn);
+  return atom$(fn) ?
+          ((fncar in NS) ? ENV.get(NS[fncar]) : apply(evl(fn, a), x, a)) :
+        eq$(fncar, LAMBDA) ?
+          evl(cadder(fn), pairlis(cadr(fn), x, a))
+
+
+
+
+          fn(...args) : apply(car(fn), ...args)) :
+          (label$(fn))
+
+}
+
+function evl()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def("ff", ff);
+def("subst", subst);
+def("equal?", equal$);
+def("nill?", nill$);
+def("empty?", empty$);
+def("append", append);
+def("member?", member$);
+def("among?", among$);
+def("pair", pair);
+def("assoc", assoc);
+def("sub2", sub2);
+def("sublis", sublis);
+def("pairlis", pairlis);
+def("quote", quote);
+def("quote?", quote$);
